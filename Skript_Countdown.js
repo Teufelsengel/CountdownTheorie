9cd72e568f218d2e58d35399e632ea72c3cbf154
JuliaDruschke <julia.druschke@deutschebahn.com>
2025-04-24 18:03:10 +0200

    Vorlesungscounter

--- Skript_Countdown.js
+++ Skript_Countdown.js
@@ -78,8 +78,6 @@ function ToggleElement(elementName) {
 
 }
 
-
-
 //Fortschrittsbalken
 var start = new Date("Apr 14, 2025 00:00:00").getTime();
 var geschafft = now - start;
@@ -90,4 +88,67 @@ var gerundet = Math.round(prozentZeit);
 window.onload = () => {
     document.getElementById("fortschritt").setAttribute("width", prozentZeit + "vw");
     document.getElementById("prozentanzeige").innerHTML = "Wir haben schon ~" + gerundet + "% geschafft.";
+    document.getElementById("vorlesungscounter").innerHTML = "In diesen Tagen haben wir noch: " + vorlesungen + " Vorlesungen.";
 };
+
+//Liste  mit Vorlesungsterminen
+const terminliste = [
+    "2025-04-24T17:30",
+    "2025-04-25T10:30",
+    "2025-04-25T15:00",
+    "2025-04-28T12:30",
+    "2025-04-28T15:00",
+    "2025-04-29T16:00",
+    "2025-04-30T12:00",
+    "2025-04-30T16:00",
+    "2025-05-05T12:30",
+    "2025-05-05T16:00",
+    "2025-05-06T15:00",
+    "2025-05-07T12:00",
+    "2025-05-08T14:00",
+    "2025-05-09T15:00",
+    "2025-05-12T12:30",
+    "2025-05-12T16:00",
+    "2025-05-13T12:30",
+    "2025-05-14T12:00",
+    "2025-05-14T16:00",
+    "2025-05-15T14:00",
+    "2025-05-16T15:00",
+    "2025-05-19T12:30",
+    "2025-05-19T16:00",
+    "2025-05-20T12:30",
+    "2025-05-20T15:00",
+    "2025-05-22T14:00",
+    "2025-05-26T12:15",
+    "2025-05-26T16:00",
+    "2025-05-27T12:30",
+    "2025-05-27T15:00",
+    "2025-06-02T12:15",
+    "2025-06-02T16:00",
+    "2025-06-03T12:30",
+    "2025-06-03T16:30",
+    "2025-06-04T12:00",
+    "2025-06-04T16:45",
+    "2025-06-05T14:00",
+    "2025-06-06T12:15",
+    "2025-06-06T16:00",
+    "2025-06-10T12:30",
+    "2025-06-11T12:00",
+    "2025-06-12T14:00",
+    "2025-06-13T13:30",
+    "2025-06-16T12:00",
+    "2025-06-17T12:30",
+    "2025-06-18T12:00",
+    "2025-06-23T12:00",
+    "2025-06-24T12:30",
+    "2025-06-24T16:30",
+    "2025-06-25T12:00",
+    "2025-06-26T14:00",
+    "2025-06-27T11:00",
+    "2025-07-01T12:30",
+];
+
+var vorlesungen = terminliste.filter(dateStr => {
+    const date = new Date(dateStr);
+    return date > now;
+}).length;
